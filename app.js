cbc1f7d3372baebf4772adbcbbaff75664597b2a
Glen Ochieng <glen.ochieng'@strathmore.edu>
2026-08-16 11:12:08 +0300

    render book cards

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   setupNavToggle();
+  renderBooks(books);
 });
 
 function setupNavToggle() {
@@ -15,4 +16,37 @@ function setupNavToggle() {
   toggle.addEventListener("click", function () {
     nav.classList.toggle("is-open");
   });
+}
+
+function renderBooks(list) {
+  var grid = document.getElementById("bookGrid");
+  var count = document.getElementById("resultCount");
+
+  if (!grid) {
+    return;
+  }
+
+  grid.innerHTML = "";
+
+  list.forEach(function (book) {
+    var card = document.createElement("article");
+    var initials = book.title.charAt(0) + book.author.charAt(0);
+    var status = book.available ? "Out on loan" : "Available";
+
+    card.className = "book-card";
+    card.innerHTML =
+      '<div class="book-cover">' + initials + "</div>" +
+      '<div class="book-body">' +
+      "<h3>" + book.title + "</h3>" +
+      '<p class="book-author">' + book.author + "</p>" +
+      '<p class="book-meta">' + book.genre + " &middot; " + book.year + "</p>" +
+      '<span class="badge">' + status + "</span>" +
+      "</div>";
+
+    grid.appendChild(card);
+  });
+
+  if (count) {
+    count.textContent = list.length + " titles in the collection";
+  }
 }
